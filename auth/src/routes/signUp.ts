1b88627bf68e97bdc99c3fd482479ffaf0d77688
Dr.Alwin Simon <003alwin@gmail.com>
2023-09-28 19:42:48 +0530

    Completed basic configuration of signup route with a validation to request data

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hello World!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Provide a valid Email"),
+    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-export { router as signUpRouter }
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log(`Created user with Email: ${email}`);
+
+    res.status(201).send(`Created user with Email: ${email}`);
+  }
+);
+
+export { router as signUpRouter };
