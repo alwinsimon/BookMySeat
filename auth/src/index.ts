71934cc50a4ef1e7f5ef7b0211d145473e39c393
Dr.Alwin Simon <003alwin@gmail.com>
2023-09-28 15:29:55 +0530

    Added more routes and files associated with it.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signIn";
+import { signOutRouter } from "./routes/signOut";
+import { signUpRouter } from "./routes/signUp";
+
 const app = express();
 
 app.use(json());
 
 const PORT = 3000;
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hello World!");
-});
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
 
 app.listen(PORT, () => {
   console.log(`AUTH SERVICE listening on PORT: ${PORT} !!!!!`);
