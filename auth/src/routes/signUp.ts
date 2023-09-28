8b0cd2fc0c6765ccfcca18e7e70b857114cdee47
Dr.Alwin Simon <003alwin@gmail.com>
2023-09-28 15:31:07 +0530

    Bug fix

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/api/users/signup", (req, res) => {
+router.post("/api/users/signup", (req, res) => {
   res.send("Hello World!");
 });
 
