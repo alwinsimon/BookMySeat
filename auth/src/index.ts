05029379d217b9a21578bceaea0ccb3ac5709741
Dr. Alwin Simon <003alwin@gmail.com>
2023-09-29 21:44:20 +0530

    Installed express-async-errors package to handle errors in async functions without the need of calling next() in express

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors"; // Package used to handle async errors
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
@@ -9,29 +10,24 @@ import { signUpRouter } from "./routes/signUp";
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 
-
 const app = express();
 
 app.use(json());
 
 const PORT = 3000;
 
-
 app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-
-
-// Resource Not Found Error Configuration 
-app.all('*', ()=> {
+// Resource Not Found Error Configuration
+app.all("*", () => {
   throw new NotFoundError();
 });
 
 // Custom Error Handler Configuration
 app.use(errorHandler);
-
 
 app.listen(PORT, () => {
   console.log(`AUTH SERVICE listening on PORT: ${PORT} !!!!!`);
