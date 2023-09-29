b2752f39f8522742a1753290258ff19e707532b8
Dr. Alwin Simon <003alwin@gmail.com>
2023-09-29 12:34:08 +0530

    Added a custom error handling middleware and done basic configuration

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+
+    res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
+  res.status(400).send({
+    errors: [{ message: err.message }]
+  });
+};
