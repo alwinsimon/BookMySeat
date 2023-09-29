85c1de3c318573c5107f01d56077709f580ed969
Dr. Alwin Simon <003alwin@gmail.com>
2023-09-29 19:22:25 +0530

    Extended abstract error class in all other error classes.

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,11 +1,20 @@
-export class DatabaseConnectionError extends Error {
-  reason = "Error connecting to database!!!";
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  
+  statusCode = 500;
+  
+  reason = 'Error connecting to database!!!';
 
   constructor() {
     // Calling the parent class (Error) constructor as this is a constructor of sub-class
-    super();
+    super('Error connecting to database!!!');
 
     // Since we are extending a built-in class (Error) the following line of cade has to be added
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors(){
+    return [ { message: this.reason} ]
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,15 +1,28 @@
+import { CustomError } from "./custom-error";
 import { ValidationError } from "express-validator";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+
+  statusCode = 400;
   
   constructor(public errors: ValidationError[]) {
     
     // Calling the parent class (Error) constructor as this is a constructor of sub-class
-    super();
+    super('Invalid Login Parameters.');
 
     // Since we are extending a built-in class (Error) the following line of cade has to be added
     Object.setPrototypeOf(this, RequestValidationError.prototype);
     
+  }
+
+  serializeErrors() {
+    return this.errors.map( (error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+
+      return { message: error.msg };
+    });
   }
 
 }
