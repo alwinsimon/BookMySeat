28fc0c401d4519111d05a2592f6c1491aae7998c
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-03 13:08:58 +0530

    Configured Password hashing logic in User Model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties that are required to create a new user
 interface UserAttrs {
@@ -26,6 +27,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const newHashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", newHashedPassword);
+  }
+  done();
 });
 
 userSchema.statics.build = (attributes: UserAttrs) => {
