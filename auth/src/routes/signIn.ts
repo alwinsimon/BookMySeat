127afe5fe1c44b5119a35898d556e91ff09132a2
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-04 23:11:28 +0530

    Configured sign-in route to find user from DB and authenticate

--- auth/src/routes/signIn.ts
+++ auth/src/routes/signIn.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
 
+import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
+
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -15,8 +20,36 @@ router.post(
       .withMessage("A password must be provided."),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.status(200).send("Hello World!");
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials.");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid Credentials.");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // Store JWT on session object (for cookie-session middleware to create a cookie with jwt)
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
