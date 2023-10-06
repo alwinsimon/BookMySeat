77d7703bfc9a13d357262edf88cef288d1400e94
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-06 12:41:30 +0530

    Added a global funtion testUserSignUp to sign-up a user and return a cookie so that it can be used in various testing files

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+
+// Declaring the function testUserSignUp to the global scope so that it can be accessed anywhere within this application
+declare global {
+  var testUserSignUp: () => Promise<string[]>;
+}
 
 let mongo: any; // Declaring it in the beginning to avoid scope issues while using inside different functions.
 
@@ -36,3 +43,19 @@ afterAll(async () => {
   // Close the mongoose connection
   await mongoose.connection.close();
 });
+
+// Create a global function to be used in tests for sign-up a test user and get a cookie to be used in the test
+// This avoids repetition of sign-up logic in multiple tests for obtaining cookie for authenticated user test.
+global.testUserSignUp = async () => {
+  const email = "tester@test.com";
+  const password = "password@123";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: email, password: password })
+    .expect(201);
+
+  const cookieFromSignUp = response.get("Set-Cookie");
+
+  return cookieFromSignUp;
+};
