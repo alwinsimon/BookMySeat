599d87e133a74491afb752431c30ac26806327e5
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-06 12:53:15 +0530

    Added a new test to check response for non-authenticated user in current-user routr

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ import { response } from "express";
 
 it("Current-User Route Test: Check for Valid User Data Return - Returns current user data on success.", async () => {
   // Use the testUserSignUp global function written in setup.ts file in test directory to get a cookie
-  const cookieFromSignUp = await global.testUserSignUp()
+  const cookieFromSignUp = await global.testUserSignUp();
   // GET request to current user route with cookie
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -13,4 +13,13 @@ it("Current-User Route Test: Check for Valid User Data Return - Returns current 
     .send()
     .expect(200);
   expect(response.body.currentUser.email).toEqual("tester@test.com");
+});
+
+it("Current-User Route Test: Un-authenticated User - Returns currentUser data as null on success.", async () => {
+  // GET request to current user route with-out cookie
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
 });
