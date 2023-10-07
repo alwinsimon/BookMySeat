825f904f81c4369c09a1c186fa42ffe6377bdd54
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-07 21:14:03 +0530

    Added a basic Sign-Up form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
