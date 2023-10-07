6fb1676b5b26f43ac811f5ec040789ec36a07be6
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-07 23:22:30 +0530

    Created a custom useRequest Hook

---
+++ client/hooks/use-request.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default ({ url, method, body }) => {
+  const [errors, setErrors] = useState(null);
+
+  const makeRequest = async () => {
+    try {
+      setErrors(null); // Setting error to null initially to prevent errors from being displayed always.
+
+      const response = await axios[method](url, body);
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h5>Oops!!!</h5>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return [ errors, makeRequest ]
+};
