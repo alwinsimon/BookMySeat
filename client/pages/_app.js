c53dca2d2a34bad5a79f34bc2ab0d799e642f728
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-09 13:42:58 +0530

    Configured _app component with a basic header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,31 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header {currentUser?.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  try {
+    const client = buildClient(appContext.ctx);
+    const response = await client.get("/api/users/currentuser");
+
+    // If the child component has getInitialProps method, then execute it manually from here.
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+      pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return { pageProps, currentUser: response.data.currentUser };
+  } catch (err) {
+    console.error("Error in making request to get current user:", err.message);
+    return { pageProps: {}, currentUser: null };
+  }
+};
+
+export default AppComponent;
