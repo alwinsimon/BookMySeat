061571ef11c2ae0427ca4d28f8db74572d9f33d4
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-10 21:04:25 +0530

    Added validation for create new ticket request handler

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@bookmyseat/common";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@bookmyseat/common";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(201);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Valid Title is Required."),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Valid Price which is greater than 0 is Required"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(201);
+  }
+);
 
 export { router as createTicketRouter };
