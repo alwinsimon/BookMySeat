39c78c2b60fa0ecc8385704864838920a07c0ce5
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-11 17:59:41 +0530

    Configured updateTicket Router in app.ts

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import { errorHandler, NotFoundError, currentUser } from "@bookmyseat/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 // Resource Not Found Error Configuration
 app.all("*", () => {
