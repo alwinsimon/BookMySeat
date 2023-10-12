259c9bfc45a75953ee3187b69f26dac211ecbf0d
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-12 13:45:28 +0530

    Configured Listener to listen for event

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,9 +1,15 @@
-import nats from 'node-nats-streaming';
+import nats from "node-nats-streaming";
 
-const stan = nats.connect('ticketing', '12345', {
-  url: 'http://localhost:4222',
+const stan = nats.connect("ticketing", "12345", {
+  url: "http://localhost:4222",
 });
 
-stan.on('connect', () => {
-  console.log('Listner connected to NATS');
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", (msg) => {
+    console.log("Message received.");
+  });
 });
