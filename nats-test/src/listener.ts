13c751eebb025806c5ca5677a40aeb0a9230aef9
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-12 15:36:06 +0530

    Configured Data reading from message

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "12345", {
   url: "http://localhost:4222",
@@ -9,7 +9,15 @@ stan.on("connect", () => {
 
   const subscription = stan.subscribe("ticket:created");
 
-  subscription.on("message", (msg) => {
-    console.log("Message received.");
+  subscription.on("message", (msg: Message) => {
+    console.log(`Message received: ${msg.getSubject()}`);
+
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(
+        `Received Event #${msg.getSequence()}, with data: ${data}`
+      );
+    }
   });
 });
