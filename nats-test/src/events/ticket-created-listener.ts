a5cc6fb32b28883602611db7746c084b6f4e2549
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-13 16:36:45 +0530

    Added type for data property of onMessage function

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -6,7 +6,7 @@ import { Subjects } from "./subjects";
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log(`Received Event with Data:`, data);
 
     msg.ack();
