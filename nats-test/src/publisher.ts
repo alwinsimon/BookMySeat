dd2cd12d1d837a739c627fa9ff0a770877a403a0
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-13 17:55:57 +0530

    Configured ticket-created publisher class in Publisher.ts

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
@@ -7,13 +8,13 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  const data = {
     id: '123',
     title: 'concert',
     price: 20,
-  });
+  };
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+
+  publisher.publish(data);
 });
