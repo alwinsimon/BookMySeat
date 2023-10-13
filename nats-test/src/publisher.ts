9f74453af0d80eee4678d6b6e93c271da5d843b4
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-13 18:10:30 +0530

    Configured Publisher with async/await syntax

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,20 +1,22 @@
-import nats from 'node-nats-streaming';
-import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
-const stan = nats.connect('ticketing', 'abc', {
-  url: 'http://localhost:4222',
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
 });
 
-stan.on('connect', () => {
-  console.log('Publisher connected to NATS');
-
-  const data = {
-    id: '123',
-    title: 'concert',
-    price: 20,
-  };
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
 
   const publisher = new TicketCreatedPublisher(stan);
 
-  publisher.publish(data);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
