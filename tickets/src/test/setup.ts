fe9b1775f0c632dbc69921ec705bbaef0fd5c283
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-14 18:14:38 +0530

    Fixed a minor issue with the directory path

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -11,7 +11,7 @@ declare global {
 }
 
 // Files to mock
-jest.mock("../../nats-client.ts");
+jest.mock("../nats-client.ts");
 
 let mongo: any; // Declaring it in the beginning to avoid scope issues while using inside different functions.
 
