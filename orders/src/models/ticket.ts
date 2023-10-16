bd483dc0d4443cb69d109f80d168794537a3d785
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-16 16:24:44 +0530

    Added a new method to ticket model for checking if a ticket is already reserved or not

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Order, OrderStatus } from "./order";
 
 // An interface that describes the properties that are required to create a new Ticket
 interface TicketAttrs {
@@ -10,6 +11,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 // An interface that decribes the properties that a Ticket Model has.
@@ -27,7 +29,7 @@ const ticketSchema = new mongoose.Schema(
       type: Number,
       min: 0,
       required: true,
-    }
+    },
   },
   {
     toJSON: {
@@ -41,6 +43,22 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attributes: TicketAttrs) => {
   return new Ticket(attributes);
+};
+
+ticketSchema.methods.isReserved = async function () {
+  const existingOrderWithSameTicket = await Order.findOne({
+    ticket: this, // this keyword here refers to the ticket document we called the isReserved method.
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  // Will return the a boolean value true if the ticket is already reserved and false otherwise.
+  return !!existingOrderWithSameTicket;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Order", ticketSchema);
