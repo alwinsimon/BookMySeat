babfc4b435cf3759c7e6deeecfbded9ba543bc70
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-16 12:10:58 +0530

    Made a Bug fix

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { body } from "express-validator";
+
+import { requireAuth, validateRequest } from "@bookmyseat/common";
+
+import { Order } from "../models/order";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.status(201).send({ status: "createOrderRouter" });
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      // Custom validation to check if the provided ticketId is a valid mongoDb document Id
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Valid Ticket Id must be provided."),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.status(201).send({ status: "createOrderRouter" });
+  }
+);
 
 export { router as createOrderRouter };
