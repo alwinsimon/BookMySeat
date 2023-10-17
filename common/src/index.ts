a9ea5dd3d77dee7d88cf5a270a82643fb83e2d2b
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-17 21:49:36 +0530

    Added exports for newely added code

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,7 +15,12 @@ export * from "./middlewares/validate-request";
 // Exporting contents of Events Directory
 export * from "./events/event-base-classes/base-publisher";
 export * from "./events/event-base-classes/base-listener";
+
 export * from "./events/event-subjects";
+
 export * from "./events/event-interfaces/ticket-created-event";
 export * from "./events/event-interfaces/ticket-updated-event";
+export * from "./events/event-interfaces/order-created-event";
+export * from "./events/event-interfaces/order-cancelled-event";
+
 export * from "./events/custom-types/order-status";
