b14e08d4dd47a5b176cd6b18d221b46aefc21cd5
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-17 21:26:59 +0530

    Added two new event subjects for orders based event

--- common/src/events/event-subjects.ts
+++ common/src/events/event-subjects.ts
@@ -1,4 +1,7 @@
 export enum EventSubjects {
   TicketCreated = "ticket:created",
-  TicketUpdated = "ticket:updated"
+  TicketUpdated = "ticket:updated",
+
+  OrderCreated = "order:created",
+  OrderCancelled = "order:cancelled",
 }
