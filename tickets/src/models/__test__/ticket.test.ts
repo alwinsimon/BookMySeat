267182438cc905a3d2636c81d4643030aec339a1
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-19 20:12:39 +0530

    Added a test to make sure that the ticket record version is incremented in each save

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -12,19 +12,19 @@ it("Successfully Implements Optimistic Concurrency Control", async () => {
   await ticket.save();
 
   // Fetch the same ticket twice (in different variables)
-  const firstFetchedTicket = await Ticket.findById(ticket.id);
-  const secondFetchedTicket = await Ticket.findById(ticket.id);
+  const firstticket = await Ticket.findById(ticket.id);
+  const secondticket = await Ticket.findById(ticket.id);
 
   // Make Two Separate Changes to the Ticket
-  firstFetchedTicket!.set({ price: 10 });
-  secondFetchedTicket!.set({ price: 20 });
+  firstticket!.set({ price: 10 });
+  secondticket!.set({ price: 20 });
 
   // Save changes to the first fetched ticket and expect a successful SAVE
-  await firstFetchedTicket!.save();
+  await firstticket!.save();
 
   // Save changes to the Second fetched ticket and expect a error while SAVE - Indicating version mismatch
   try {
-    await secondFetchedTicket!.save();
+    await secondticket!.save();
   } catch (err) {
     return;
   }
@@ -33,4 +33,37 @@ it("Successfully Implements Optimistic Concurrency Control", async () => {
   throw new Error(
     "Optimistic Concurrency Control Test Failed if the test reached This line of code"
   );
+});
+
+it("Successfully Increments the Version Number on Multiple Manipulation of same record", async () => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "Sample Ticket",
+    price: 5,
+    userId: "sampleUserID",
+  });
+
+  // Save the ticket to DB
+  await ticket.save();
+
+  // Expect the version number to be Zero
+  expect(ticket.version).toEqual(0);
+
+  // Update the ticket once
+  ticket.set({ price: 10 });
+
+  // Save changes to the ticket and expect a successful SAVE
+  await ticket.save();
+
+  // Expect he version number to be incremented once
+  expect(ticket.version).toEqual(1);
+
+  // Update the ticket again
+  ticket.set({ price: 20 });
+
+  // Save changes to the ticket and expect a successful SAVE
+  await ticket.save();
+
+  // Expect he version number to be incremented once
+  expect(ticket.version).toEqual(2);
 });
