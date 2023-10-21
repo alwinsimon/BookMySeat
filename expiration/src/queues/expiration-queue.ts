d444221e25e1f315dc5b12ce908c8e0f3c0343d8
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-21 23:25:10 +0530

    Added a expiration queue using BullJs.

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,19 @@
+import Queue from "bull";
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log(
+    `Publish a expiration:complete event for orderId: ${job.data.orderId}`
+  );
+});
+
+export { expirationQueue };
