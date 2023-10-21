5315a9e38de9ddd2fda649aabbcd340de2dea13a
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-21 23:36:08 +0530

    Configured Expiration Queue in order-created-listener.

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Message } from "node-nats-streaming";
 import { Listener, EventSubjects, OrderCreatedEvent } from "@bookmyseat/common";
 import { queueGroupName } from "../expiration-service-queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = EventSubjects.OrderCreated;
@@ -9,6 +10,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     // Process the data
+    await expirationQueue.add({ orderId: data.id });
 
     // Acknowledge the event
     msg.ack();
