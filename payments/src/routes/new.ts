4009f92bdcb911e030c10a9484a9b4568b03d194
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-25 13:05:24 +0530

    Added some validations for payment creation request.

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,8 +4,11 @@ import {
   requireAuth,
   validateRequest,
   NotFoundError,
+  NotAuthorizedError,
   BadRequestError,
+  OrderStatus,
 } from "@bookmyseat/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -21,6 +24,20 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot create a payment for cancelled order.");
+    }
+
     res.status(201).send({ chargeCreation: "Success" });
   }
 );
