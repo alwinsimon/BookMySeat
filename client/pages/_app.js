796aea5dc976c3f83d465a4d14e8509effcf0f23
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-27 16:47:46 +0530

    Fixed a reassignment bug.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -25,7 +25,7 @@ AppComponent.getInitialProps = async (appContext) => {
       pageProps = await appContext.Component.getInitialProps(
         appContext.ctx,
         client,
-        data.currentUser
+        response.data.currentUser
       );
     }
 
