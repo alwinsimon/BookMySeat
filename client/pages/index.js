8ee9d2262e92cea96e8d7f8c791357f6313aebc2
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-27 12:41:02 +0530

    Modified to remove fetching current user.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,9 @@
-import buildClient from "../api/build-client";
-
 const IndexPage = ({ currentUser }) => {
   if (!currentUser) {
     return (
       <>
         <h1>Landing Page</h1>
-        <br/>
+        <br />
         <h5>Signed Out</h5>
       </>
     );
@@ -21,14 +19,7 @@ const IndexPage = ({ currentUser }) => {
 };
 
 IndexPage.getInitialProps = async (context) => {
-  try {
-    const client = buildClient(context);
-    const response = await client.get("/api/users/currentuser");
-    return response.data;
-  } catch (err) {
-    console.error("Error in making request to get current user:", err.message);
-    return { currentUser: null };
-  }
+  return {};
 };
 
 export default IndexPage;
