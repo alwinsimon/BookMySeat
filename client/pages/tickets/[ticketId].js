6e8c1685df6043435cdb6322db323050fb28c92d
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-27 18:30:08 +0530

    Added purchase button.

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,39 @@
-const TicketShow = () => {
-  return <div>TicketShow</div>;
+import useRequest from "../../hooks/use-request";
+
+const TicketShow = ({ ticket }) => {
+  const [makeRequest, errors] = useRequest({
+    url: "/api/orders",
+    method: "post",
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  const handleClick = async () => {
+    makeRequest();
+  };
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price} INR</h4>
+      {errors}
+      <button className="btn btn-primary" onClick={handleClick}>
+        Purchase
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
 };
 
 export default TicketShow;
