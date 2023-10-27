9a61a40207a4fa45a9d29e593e8cb752ebe5b5b6
Dr. Alwin Simon <003alwin@gmail.com>
2023-10-27 21:47:56 +0530

    Added order expiration Timer.

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,32 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>;
+import { useEffect, useState } from "react";
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const milliSecondsLeft = new Date(order.expiresAt) - new Date();
+      const secondsLeft = Math.round(milliSecondsLeft / 1000);
+
+      setTimeLeft(secondsLeft);
+    };
+
+    findTimeLeft();
+    const timer = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [order]);
+
+  return <div>Time left to complete payment: {timeLeft} seconds.</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 };
 
 export default OrderShow;
